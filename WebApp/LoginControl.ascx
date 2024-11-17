fdceee25dcbb0131dd624a0cc35e84e4b7c8e003
Yurii Malikov <yurii.malikov@northrockllc.com>
2024-11-17 23:02:01 +0200

    Login control

---
+++ WebApp/LoginControl.ascx
@@ -0,0 +1,18 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="LoginControl.ascx.cs" Inherits="TryItWebApplication.LoginControl" %>
+
+<div>
+    <h2>Login</h2>
+    <label for="txtLoginUserId">User ID (user1 or user2 or user3):</label><br />
+    <asp:TextBox ID="txtLoginUserId" runat="server" Width="300px" Text="user1"></asp:TextBox><br />
+
+    <label for="txtPassword">Password (pass1 or pass2 or pass3):</label><br />
+    <asp:TextBox ID="txtPassword" runat="server" Width="300px" Text="pass1" TextMode="Password"></asp:TextBox><br />
+
+    <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" /><br />
+
+    <!-- Logout Button -->
+    <asp:Button ID="btnLogout" runat="server" Text="Logout" OnClick="btnLogout_Click" /><br />
+
+    <asp:Label ID="lblLoginMessage" runat="server" ForeColor="Red"></asp:Label>
+</div>
+<hr />
---
+++ WebApp/LoginControl.ascx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.ChatGPTServiceReference;
+
+namespace TryItWebApplication
+{
+    public partial class LoginControl : System.Web.UI.UserControl
+    {
+        public event EventHandler LoginStatusChanged;
+
+        private Dictionary<string, string> registeredUsers;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Always load registered users
+            registeredUsers = Application["registeredUsers"] as Dictionary<string, string>;
+
+            if (!IsPostBack)
+            {
+                if (Session["userId"] != null)
+                {
+                    lblLoginMessage.Text = $"Welcome, {Session["userId"]}!";
+                    lblLoginMessage.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    lblLoginMessage.Text = "Please log in.";
+                    lblLoginMessage.ForeColor = System.Drawing.Color.Red;
+                }
+
+                UpdateLoginLogoutVisibility();
+            }
+        }
+
+        private void UpdateLoginLogoutVisibility()
+        {
+            if (Session["userId"] != null)
+            {
+                // User is logged in
+                btnLogin.Visible = false;
+                txtLoginUserId.Visible = false;
+                txtPassword.Visible = false;
+                btnLogout.Visible = true;
+            }
+            else
+            {
+                // User is not logged in
+                btnLogin.Visible = true;
+                txtLoginUserId.Visible = true;
+                txtPassword.Visible = true;
+                btnLogout.Visible = false;
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userId = txtLoginUserId.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (registeredUsers != null && registeredUsers.ContainsKey(userId))
+            {
+                if (registeredUsers[userId] == password)
+                {
+                    // Login successful
+                    Session["userId"] = userId;
+                    lblLoginMessage.Text = $"Welcome, {userId}!";
+                    lblLoginMessage.ForeColor = System.Drawing.Color.Green;
+                    OnLoginStatusChanged();
+                }
+                else
+                {
+                    lblLoginMessage.Text = "Incorrect password.";
+                    lblLoginMessage.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            else
+            {
+                lblLoginMessage.Text = "Login failed. You are not registered.";
+                lblLoginMessage.ForeColor = System.Drawing.Color.Red;
+            }
+
+            UpdateLoginLogoutVisibility();
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            // Clear the session
+            Session["userId"] = null;
+            lblLoginMessage.Text = "You have been logged out.";
+            lblLoginMessage.ForeColor = System.Drawing.Color.Red;
+            OnLoginStatusChanged();
+            UpdateLoginLogoutVisibility();
+        }
+
+        protected void OnLoginStatusChanged()
+        {
+            if (LoginStatusChanged != null)
+            {
+                LoginStatusChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
